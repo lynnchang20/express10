1aa61a4edb76ba599dd1c9be586a45307f25482e
Lynn <lynnchang20@gmail.com>
2024-03-12 16:21:27 +0800

    1. change index.mjs to index.js 2. change package.json 3. add readme

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-import express from 'express';
-import { resolve } from 'path';
-const __dirname = import.meta.dirname;
+// import express from 'express';
+// import { resolve } from 'path';
+const express = require('express');
+const { resolve } = require('path');
+
 
 const app = express();
 app.use(express.static(resolve(__dirname, 'public')));
